e7029a6e5216eb17c45691140c93f5928b3ad2ca
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-10 10:22:20 +0300

    Add single round function

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function computerPlay() {
   switch(Math.floor(Math.random() * 3)) {
-    case 0: return "rock";
-    case 1: return "paper";
-    case 2: return "scissors";
+    case 0: return "Rock";
+    case 1: return "Paper";
+    case 2: return "Scissors";
   }
 }
 
 function userPlay() {
-  return prompt("Enter your move!").toLowerCase();
+  const userInput = prompt("Enter your move!").toLowerCase();
+  return userInput.charAt(0).toUpperCase() + userInput.slice(1);
 }
 
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection === "Rock" && computerSelection === "Scissors"
+    || playerSelection === "Paper" && computerSelection === "Rock"
+    || playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! " + playerSelection + " beats " + computerSelection; 
+  }
+  else if(playerSelection === "Rock" && computerSelection === "Paper"
+    || playerSelection === "Paper" && computerSelection === "Scissors"
+    || playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! " + computerSelection + " beats " + playerSelection; 
+  }
+  return "Tie!";
+}
